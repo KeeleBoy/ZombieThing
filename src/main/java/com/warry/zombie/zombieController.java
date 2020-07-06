5d087bbece70fb01258bc20cb0a8b333521c5bf1
kyle <kwarchuck@gmail.com>
2020-07-06 14:57:28 -0400

    First commit

---
+++ src/main/java/com/warry/zombie/zombieController.java
@@ -0,0 +1,20 @@
+package com.warry.zombie;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class zombieController {
+	@Autowired
+	private HttpSession sesh;
+
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("home");
+		return mav;
+	}
+}
